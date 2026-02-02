fe20158b138462ddfac2ef37bc8d57b18f77c9bd
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-02 13:50:30 +0300

    Refactoring AddPostgreSqlDbContext

---
+++ StoreApi/Extensions/PostgreSqlServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApi.Data;
+
+namespace StoreApi.Extensions;
+
+public static class PostgreSqlServiceExtensions
+{
+    public static void AddPostgreSqlDbContext(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("PostgreSQLConnection"));
+        });
+    }
+
+}
--- StoreApi/Program.cs
+++ StoreApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using StoreApi.Data;
+using StoreApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    {
-        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection"));
-    });
+builder.Services.AddPostgreSqlDbContext(builder.Configuration);
 
 var app = builder.Build();
 
