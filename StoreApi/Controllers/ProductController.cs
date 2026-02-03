ef2bdc2123ed7cb23ba2724f953d4879e3848eed
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-03 12:20:12 +0300

    Add GetProductById

--- StoreApi/Controllers/ProductController.cs
+++ StoreApi/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ using StoreApi.Data;
 using StoreApi.Model;
 
 namespace StoreApi.Controllers;
-
+[Route("api/[controller]/[action]")]
 public class ProductController : StoreController
 {
     public ProductController(AppDbContext dbContext) : base(dbContext)
@@ -20,6 +20,35 @@ public class ProductController : StoreController
             StatusCode = HttpStatusCode.OK,
             Response = await dbContext.Products.ToListAsync()
         };
+        return Ok(response);
+    }
+
+    
+    [HttpGet]
+    public async Task<IActionResult> GetProductById(int id)
+    {
+
+        ResponseServer response = new ResponseServer();
+        if (id <= 0)
+        {
+            response.IsSucces = false;
+            response.StatusCode = HttpStatusCode.BadRequest;
+            response.ErrorMessages.Add("Неверно указанный id. Должен быть >= 0");
+            return BadRequest(response);
+        }
+
+        Product product = await dbContext.Products.FirstOrDefaultAsync(e => e.Id == id);
+
+        if (product is null)
+        {
+            response.IsSucces = false;
+            response.StatusCode = HttpStatusCode.NotFound;
+            response.ErrorMessages.Add("Товар по указанному id не найден");
+            return NotFound(response);
+        }
+
+        response.StatusCode = HttpStatusCode.OK;
+        response.Response = product;
         return Ok(response);
     }
 }
