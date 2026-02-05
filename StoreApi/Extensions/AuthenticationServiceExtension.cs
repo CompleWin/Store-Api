e232e6a89f274f4d3c92577460b999c6b9b83dde
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-05 12:29:20 +0300

    add AuthServiceExtension

---
+++ StoreApi/Extensions/AuthenticationServiceExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace StoreApi.Extensions;
+
+public static class AuthenticationServiceExtension
+{
+    public static IServiceCollection AddAuthenticationService(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var secretKey = configuration["AuthSettings:SecretKey"];
+
+        services.AddAuthentication(o =>
+        {
+            o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(o =>
+        {
+            o.RequireHttpsMetadata = false;
+            o.SaveToken = true;
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true
+            };
+        });
+
+        return services;
+    }
+    
+}
