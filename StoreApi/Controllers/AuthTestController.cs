e534fe3fb116c1a077ac2187ed021ecb4cfd4954
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-05 12:58:24 +0300

    Add AuthTestController

---
+++ StoreApi/Controllers/AuthTestController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StoreApi.Common;
+using StoreApi.Data;
+using StoreApi.Model;
+
+namespace StoreApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class AuthTestController : StoreController
+{
+    public AuthTestController(AppDbContext context) : base(context)
+    {
+    }
+
+    [HttpGet]
+    public IActionResult Test1()
+    {
+        return Ok(new ResponseServer
+        {
+            StatusCode = HttpStatusCode.OK,
+            Response = "Тест для всех пользователей"
+        });
+    }
+    
+    [HttpGet]
+    [Authorize]
+    public IActionResult Test2()
+    {
+        return Ok(new ResponseServer
+        {
+            StatusCode = HttpStatusCode.OK,
+            Response = "Тест для авторизованных пользователей"
+        });
+    }
+    
+    [HttpGet]
+    [Authorize(Roles = SharedData.Roles.Consumer)]
+    public IActionResult Test3()
+    {
+        return Ok(new ResponseServer
+        {
+            StatusCode = HttpStatusCode.OK,
+            Response = "Тест для Consumer пользователей"
+        });
+    }
+    
+    [HttpGet]
+    [Authorize(Roles = SharedData.Roles.Admin)]
+    public IActionResult Test4()
+    {
+        return Ok(new ResponseServer
+        {
+            StatusCode = HttpStatusCode.OK,
+            Response = "Тест для Admin пользователей"
+        });
+    }
+}
