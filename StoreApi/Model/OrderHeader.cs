9ebd12f0ab990c64677f9be0cf78329425586e8d
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-06 12:47:35 +0300

    Add OrderHeader

---
+++ StoreApi/Model/OrderHeader.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreApi.Model;
+
+public sealed class OrderHeader
+{
+    [Key]
+    public int OrderHeaderId { get; set; }
+    [Required]
+    public string CustomerName { get; set; }
+    [Required]
+    public string CustomerEmail { get; set; }
+
+    public string AppUserId { get; set; }
+    [ForeignKey(nameof(AppUserId))]
+    public AppUser User { get; set; }
+
+    public double OrderTotalAmount { get; set; }
+    public DateTime OrderDateTime { get; set; }
+    public int TotalCount { get; set; }
+    public IEnumerable<OrderDetails> OrderDetailsItems { get; set; }
+}
