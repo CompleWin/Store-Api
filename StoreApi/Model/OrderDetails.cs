f262d08e6639d9d5acf91ca2518ab31b94816bb3
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-06 12:30:18 +0300

    Add OrderDetails model

---
+++ StoreApi/Model/OrderDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreApi.Model;
+
+public sealed class OrderDetails
+{
+    [Key]
+    public int OrderDetailId { get; set; }
+    [Required]
+    public int OrderHeaderId { get; set; }
+    [Required]
+    public int ProductId { get; set; }
+    [ForeignKey(nameof(ProductId))]
+    public Product Product { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public string ItemName { get; set; }
+    [Required]
+    public double Price { get; set; }
+}
